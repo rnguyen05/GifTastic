6b58b7fd3d4289727a0adba03c163b92f8874e88
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-03-31 08:57:14 -0700

    Add jumbotron class to header in index.html, make html responsive, allow user load more gifs and append to bottom, add title to every gif to animals.js

--- assets/javascript/animals.js
+++ assets/javascript/animals.js
@@ -3,38 +3,113 @@ var addNewAnimal;
 var topics = ["cat", "dog", "rabbit", "hamster", "chicken", "duck", "goat", "cow", "donkey"];
 var apiKey = "&api_key=BcWt5JuyihFp3RM11Nr17BruDdYTsu4q";
 var queryAnimal;
-var loadMore;
-var queryUrlBase = "http://api.giphy.com/v1/gifs/search?limit=10&q=";
+var loadMore = 25;
+var clickCount = 1;
+var limit = "&limit=10";
+var queryUrlBase = "http://api.giphy.com/v1/gifs/search?&q=";
 //========================================================
 //Methods
+//Function to convert first char in a word to uppercase
+function toTitleCase (str)
+{
+    return str.replace(/\w\S*/g, function (txt)
+    {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
 //Button click event listener to call ajax to display images
 $("#displayBtn").on('click', '.btn', function () {
     $("#displayAnimals").empty();
-    var queryAnimal = $(this).val();
+    queryAnimal = $(this).val();
     $.ajax({
-        url: queryUrlBase + queryAnimal + apiKey,
+        url: queryUrlBase + queryAnimal + apiKey + limit,
         method: "GET"
     }).then(function(response) {
         //Display ajax results
         var results = response.data;
-
+        console.log(results);
         for (var d = 0; d < results.length; d++) {
             var gifDiv = $("<div id='BtnImg' class='mr-3 mb-4'>");
-            var gifRating = "Rating: "+results[d].rating;
+            
+            //Gif title
+            var gifTitle = results[d].title;
+            gifTitle = gifTitle.substring(0, gifTitle.indexOf('GIF'));
+            //Gif Rating
+            var gifRating = "<br />Rating: "+results[d].rating;
+            //Gif download button
+            var downloadBtn = $("<button>");
+            downloadBtn.css("float","right");
+            downloadBtn.addClass("btn btn-link btn-sm");
+            downloadBtn.attr("href",results[d].url);
+            downloadBtn.html("Download");
+
+            gifDiv.append(toTitleCase(gifTitle));
+            gifDiv.append(gifRating);
+            gifDiv.append(downloadBtn);
+
             var gifImg = $("<img>");
             gifImg.attr("src",results[d].images.fixed_height_still.url);
             gifImg.attr("data-still",results[d].images.fixed_height_still.url);
             gifImg.attr("data-animate",results[d].images.fixed_height.url);
             gifImg.attr("data-state","still");
             gifImg.attr("class","gifImg");
-            
-            gifDiv.append(gifRating);
             gifDiv.append(gifImg);
+
             $("#displayAnimals").append(gifDiv);   
         }
-        
-
+        //Display load more option button
+        $("#loadMore").css("display", "block");
     });
+});
+
+//If loadMoreBtn clicked
+$("#loadMore").on('click', function () {
+    var offset = "&offset="+loadMore*clickCount;
+    $.ajax({
+        url: queryUrlBase + queryAnimal + apiKey + limit + offset,
+        method: "GET"
+    }).then(function(response) {
+        //Display ajax results
+        var results = response.data;
+        for (var d = 0; d < results.length; d++) {
+            var gifDiv = $("<div id='BtnImg' class='mr-3 mb-4'>");
+            //Display gif title
+            var gifTitle = results[d].title;
+            gifTitle = gifTitle.substring(0, gifTitle.indexOf('GIF'));
+            //Gif Rating
+            var gifRating = "<br />Rating: "+results[d].rating;
+            //Gif download button
+            var downloadBtn = $("<button>");
+            downloadBtn.css("float","right");
+            downloadBtn.addClass("btn btn-link btn-sm");
+            downloadBtn.attr("href",results[d].images.fixed_height.url);
+            downloadBtn.html("Download");
+            //Append to displayAnimals div
+            gifDiv.append(toTitleCase(gifTitle));
+            gifDiv.append(gifRating);
+            gifDiv.append(downloadBtn);
+
+            var gifImg = $("<img>");
+            gifImg.attr("src",results[d].images.fixed_height_still.url);
+            gifImg.attr("data-still",results[d].images.fixed_height_still.url);
+            gifImg.attr("data-animate",results[d].images.fixed_height.url);
+            gifImg.attr("data-state","still");
+            gifImg.attr("class","gifImg");
+            gifDiv.append(gifImg);
+
+            $("#displayAnimals").append(gifDiv);   
+        }
+        //Display load more option button
+        $("#loadMore").css("display", "block");
+        console.log(loadMore);
+    });
+    clickCount++;
+    
+});
+
+//Download button click event listener
+$(".btn-link").on('click', function () {
+
 });
 
 //Button click event listener to pause gif images
@@ -69,7 +144,7 @@ function addNewBtn (newElement) {
 };
 
 //Add an Animal button click event listener
-$(".btn").on('click', function (e) {
+$(".btn-outline-secondary").on('click', function (e) {
     addNewAnimal = $("#newAnimal").val().toLowerCase().trim();
     if (addNewAnimal.length !== 0) {
         addNewBtn(addNewAnimal);
