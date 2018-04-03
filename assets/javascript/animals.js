14b6445af8c65bad134fc46ce92ce46ade73db74
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.att.net>
2018-04-03 12:44:59 -0700

    Added localStorage to Favorite gifs, make app is responsive, allow users to add load additional gifs(10 gifs at a time), add favorite, download and title for gif

--- assets/javascript/animals.js
+++ assets/javascript/animals.js
@@ -1,8 +1,10 @@
 //Variables
 var addNewAnimal;
 var topics = ["cat", "dog", "rabbit", "hamster", "chicken", "duck", "goat", "cow", "donkey"];
+var favorites = [];
 var apiKey = "&api_key=BcWt5JuyihFp3RM11Nr17BruDdYTsu4q";
 var queryAnimal;
+var offset = 0;
 var loadMore = 25;
 var clickCount = 1;
 var limit = "&limit=10";
@@ -17,17 +19,42 @@ function toTitleCase (str)
     });
 }
 
-//Button click event listener to call ajax to display images
-$("#displayBtn").on('click', '.btn', function () {
-    $("#displayAnimals").empty();
-    queryAnimal = $(this).val();
+//Dislay all predifined animals in array
+function displayArr () {
+    for (var i = 0; i < topics.length; i++) {
+        var newBtn = $("<button id='Btn'>")
+        newBtn.addClass("btn btn-secondary  mr-1 mb-3").attr("value",topics[i]);
+        newBtn.html(topics[i]);
+        $("#displayBtn").append(newBtn);
+    }
+    //Display favorite images from locaStorage
+    var tmp = localStorage.getItem("localStorageFav");
+    if (tmp !== null) {
+        var tmpArr = tmp.split('~');
+        for (var i = 1; i < tmpArr.length; i++) {
+            newTmp = stripQuotes(tmpArr[i]);
+            favoriteGifs(newTmp);
+        }
+    }     
+}
+
+//Add new button to display
+function addNewBtn (newElement) {
+    var newBtn = $("<button>")
+    newBtn.addClass("btn btn-secondary  mr-1 mb-3").attr("value",newElement);
+    newBtn.html(newElement);
+    $("#displayBtn").append(newBtn);
+}
+
+//Ajax function to fetch data
+function fetchData (varOffset) {
     $.ajax({
-        url: queryUrlBase + queryAnimal + apiKey + limit,
+        url: queryUrlBase + queryAnimal + apiKey + limit + varOffset,
         method: "GET"
     }).then(function(response) {
         //Display ajax results
         var results = response.data;
-        console.log(results);
+        // console.log(results);
         for (var d = 0; d < results.length; d++) {
             var gifDiv = $("<div id='BtnImg' class='mr-3 mb-4'>");
             
@@ -36,17 +63,27 @@ $("#displayBtn").on('click', '.btn', function () {
             gifTitle = gifTitle.substring(0, gifTitle.indexOf('GIF'));
             //Gif Rating
             var gifRating = "<br />Rating: "+results[d].rating;
-            //Gif download button
+            //Favorite Icon
+            var favoriteBtn = $("<button>");
+            favoriteBtn.css("float","right");
+            favoriteBtn.addClass("btn btn-fv btn-sm");
+            favoriteBtn.attr("value",results[d].images.preview_gif.url);
+            favoriteBtn.attr("title","Add to Favorites");
+            favoriteBtn.html("<i class='fas fa-heart'></i>");
+
+            //Download icon
             var downloadBtn = $("<button>");
             downloadBtn.css("float","right");
-            downloadBtn.addClass("btn btn-link btn-sm");
-            downloadBtn.attr("href",results[d].url);
-            downloadBtn.html("Download");
+            downloadBtn.addClass("btn btn-dl btn-sm");
+            downloadBtn.attr("value",results[d].images.fixed_height.url);
+            downloadBtn.attr("title","Download");
+            downloadBtn.html("<i class='fas fa-download'></i>");
 
-            gifDiv.append(toTitleCase(gifTitle));
+            gifDiv.append("<strong>"+toTitleCase(gifTitle)+"</strong>");
             gifDiv.append(gifRating);
             gifDiv.append(downloadBtn);
-
+            gifDiv.append(favoriteBtn);
+            
             var gifImg = $("<img>");
             gifImg.attr("src",results[d].images.fixed_height_still.url);
             gifImg.attr("data-still",results[d].images.fixed_height_still.url);
@@ -60,55 +97,73 @@ $("#displayBtn").on('click', '.btn', function () {
         //Display load more option button
         $("#loadMore").css("display", "block");
     });
+}
+
+//One Click download function
+function downloadGif (data) {
+    var anchor = document.createElement('a');
+    anchor.href = data.downloadUrl;
+    anchor.target = '_blank';
+    anchor.download = data.fileName;
+    anchor.click();
+}
+
+//Function to remove begin and end quotes in string
+function stripQuotes(a) {
+    if (a.charAt(0) === '"' && a.charAt(a.length-1) === '"') {
+        return a.substr(1, a.length-2);
+    }
+    return a;
+}
+
+//Write to localStorage
+function writeLocalStorage(data) {
+    var old = localStorage.getItem("localStorageFav");
+    if (old === null) {old = ""};
+    localStorage.setItem("localStorageFav",  old + "~" + data);
+    console.log(localStorage);
+}
+
+//Add to Favorite Gifs function
+function favoriteGifs (addToFav) {
+    if (favorites.indexOf(addToFav) == -1) {    
+        favorites.push(addToFav);
+        
+        var newImg = $("<img>")
+        newImg.attr("src",addToFav);
+        newImg.attr("width","50");
+        newImg.attr("height","50");
+        newImg.css("margin","0px 2px 3px");
+        $("#displayFav").append(newImg);  
+    }
+}
+
+//Save to Favorites Event Listener
+$("#displayAnimals").on('click', '.btn-fv', function () {
+    var addToFav = $(this).val();
+    $(".Fav").css("display","block");
+    writeLocalStorage(JSON.stringify(addToFav));
+    favoriteGifs(addToFav);//take this out later
 });
 
-//If loadMoreBtn clicked
+//Button click event listener to call ajax to display images
+$("#displayBtn").on('click', '.btn', function () {
+    $("#displayAnimals").empty();
+    queryAnimal = $(this).val();
+    offset = "&offset="+offset;
+    fetchData(offset);
+});
+
+//loadMore... button click event
 $("#loadMore").on('click', function () {
-    var offset = "&offset="+loadMore*clickCount;
-    $.ajax({
-        url: queryUrlBase + queryAnimal + apiKey + limit + offset,
-        method: "GET"
-    }).then(function(response) {
-        //Display ajax results
-        var results = response.data;
-        for (var d = 0; d < results.length; d++) {
-            var gifDiv = $("<div id='BtnImg' class='mr-3 mb-4'>");
-            //Display gif title
-            var gifTitle = results[d].title;
-            gifTitle = gifTitle.substring(0, gifTitle.indexOf('GIF'));
-            //Gif Rating
-            var gifRating = "<br />Rating: "+results[d].rating;
-            //Gif download button
-            var downloadBtn = $("<button>");
-            downloadBtn.css("float","right");
-            downloadBtn.addClass("btn btn-link btn-sm");
-            downloadBtn.attr("href",results[d].images.fixed_height.url);
-            downloadBtn.html("Download");
-            //Append to displayAnimals div
-            gifDiv.append(toTitleCase(gifTitle));
-            gifDiv.append(gifRating);
-            gifDiv.append(downloadBtn);
-
-            var gifImg = $("<img>");
-            gifImg.attr("src",results[d].images.fixed_height_still.url);
-            gifImg.attr("data-still",results[d].images.fixed_height_still.url);
-            gifImg.attr("data-animate",results[d].images.fixed_height.url);
-            gifImg.attr("data-state","still");
-            gifImg.attr("class","gifImg");
-            gifDiv.append(gifImg);
-
-            $("#displayAnimals").append(gifDiv);   
-        }
-        //Display load more option button
-        $("#loadMore").css("display", "block");
-        console.log(loadMore);
-    });
+    offset = "&offset="+loadMore*clickCount;
+    fetchData(offset);
     clickCount++;
-    
 });
 
-//Download button click event listener
-$(".btn-link").on('click', function () {
+//Download button click event
+$("#displayAnimals").on('click', ".btn-dl", function () {
+    var gifFile = $(this).val();
 
 });
 
@@ -123,25 +178,6 @@ $("#displayAnimals").on('click', ".gifImg",function () {
         $(this).attr("data-state", "still");
         }  
 });
-
-
-//Dislay all predifined animals in array
-function displayArr () {
-    for (var i = 0; i < topics.length; i++) {
-        var newBtn = $("<button id='Btn'>")
-        newBtn.addClass("btn btn-secondary  mr-1 mb-3").attr("value",topics[i]);
-        newBtn.html(topics[i]);
-        $("#displayBtn").append(newBtn);
-    }
-};
-
-//Add new button to display
-function addNewBtn (newElement) {
-    var newBtn = $("<button>")
-    newBtn.addClass("btn btn-secondary  mr-1 mb-3").attr("value",newElement);
-    newBtn.html(newElement);
-    $("#displayBtn").append(newBtn);
-};
 
 //Add an Animal button click event listener
 $(".btn-outline-secondary").on('click', function (e) {
